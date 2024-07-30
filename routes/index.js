52ab351afbd2585123ca487e5e31c759340110ca
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-29 22:42:53 -0400

    added server,js and index routes

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+// Import modular router for notes route
+const notesRouter = require('./notes');
+
+router.use('/notes', notesRouter);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Express middleware to parse json and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
